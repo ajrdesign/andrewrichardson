3cfca3dad3c13c2b78a1ca25f263069e0e9b62b3
Andrew Richardson <andrewrichardson@Andrews-MacBook-Pro.local>
2013-10-29 20:51:46 -0700

    Deleting kirby

--- site/snippets/navigation.php
+++
@@ -1,9 +0,0 @@
-<!--Navigation-->
-<div class="header">
-	<ul id="navigation">
-		<?php foreach($pages->visible() AS $p): ?>
-		<li><a href="<?php echo $p->url() ?>" class="nav"><?php echo html($p->title()) ?></a></li>
-		<?php endforeach ?>		
-	</ul>
-</div>
-<hr />
